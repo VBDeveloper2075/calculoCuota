f472ab0ff871e98bae3e077c261ce1a75bf707f1
VBDeveloper2075 <105943703+VBDeveloper2075@users.noreply.github.com>
2024-12-01 23:20:25 -0300

    result y estilos

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function calcularCuota() {
   let venta = document.getElementById("venta").value;
   let compra = document.getElementById("compra").value;
-  let result = parseFloat(venta) - parseFloat(compra);
+  let result = [(parseFloat(venta) + parseFloat(compra))/2]*85;
   document.getElementById("result").innerHTML = "La cuota es: " + result;
-  
+  document.getElementById("result").style.display = "block";
+}
+
+function refrescar() {
+  document.getElementById("venta").value = "";
+  document.getElementById("compra").value = "";
+  document.getElementById("result").style.display = "none";
 }
